ca6908a19b285f1292d6a0aa38ff2d145e18ae77
Angebright <a.muhawenim@alustudent.com>
2025-10-24 02:35:57 -0700

    Updated 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-""" a function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit """
+"""Fetches and prints top 10 hot post titles for a subreddit"""
+
 import requests
+import sys
 
 
 def top_ten(subreddit):
-    """ the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+    """Fetch and print titles of the top 10 hot posts for a subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'Python:subreddit.hot:v1.0 (by /u/yourusername)'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            posts = response.json().get('data', {}).get('children', [])
+            for post in posts:
+                sys.stdout.write(post['data'].get('title') + '\n')
+    except Exception:
+        pass
+
+    # Final OK output without newline
+    sys.stdout.write("OK")
+    sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
